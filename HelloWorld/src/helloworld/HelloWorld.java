f964160d726c1a751b7907409e5fe01e1c2fa066
Alexander Vasil <alexandervasil840@gmail.com>
2019-08-30 15:47:25 -0400

    just added some thing to Hello World files

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -1,23 +1,23 @@
 package helloworld;
 
 /**
- *
+ * This code will print Hello World, and some ASCII Art.
  * @author Alexander Vasil
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
 //        System.out.println("Hello World");
-        System.out.println("*********");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*********");
-
+//        System.out.println("*********");
+//        System.out.println("*       *");
+//        System.out.println("*       *");
+//        System.out.println("*       *");
+//        System.out.println("*       *");
+//        System.out.println("*       *");
+//        System.out.println("*       *");
+//        System.out.println("*       *");
+//        System.out.println("*********");
+        
     }
     
 }
