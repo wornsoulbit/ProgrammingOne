6b21e136ee50063e66aa682f3e43294e8b26c028
Alexander Vasil <alexandervasil840@gmail.com>
2019-08-30 14:32:37 -0400

    Created Hello World program file for netbeans 8.2

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+package helloworld;
+
+/**
+ *
+ * @author Alexander Vasil
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        System.out.println("*********");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*********");
+
+    }
+    
+}
