8fd4c58eaeea79c4d857702ba88193649072b53f
Alexander Vasil <alexandervasil840@gmail.com>
2019-09-04 16:55:38 -0400

    created a test folder for the class excerise and hard coded the answer that was wanted

---
+++ Test/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Calculate the century and the decade based on ones birth year
+ * @author Alexander Vasil
+ */
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println(1997/100+1);  
+        System.out.println(1997%100/10*10);
+    }
+    
+}
