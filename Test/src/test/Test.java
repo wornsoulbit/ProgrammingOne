7929476b1bf9797fcb50f58f04ea7a9556167040
Alexander Vasil <alexandervasil840@gmail.com>
2019-09-04 17:24:47 -0400

    just added a line of code

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -1,14 +1,18 @@
 package test;
 
 /**
- * Calculate the century and the decade based on ones birth year
+ * Calculate the century and the decade based on ones birth year and calculating the total price of food with tips
  * @author Alexander Vasil
  */
 public class Test {
     
-    public static void main(String[] args) {
-        System.out.println(1997/100+1);  
-        System.out.println(1997%100/10*10);
+    public static void main(String[] args) {     
+//        //calculating the century
+//        System.out.println(1997/100+1);  
+//        //calculating the decade
+//        System.out.println(1997%100/10*10);
+        //calculating the total price with tips
+        System.out.println(20*(1+0.05+0.01)*(1+0.15));
     }
     
 }
