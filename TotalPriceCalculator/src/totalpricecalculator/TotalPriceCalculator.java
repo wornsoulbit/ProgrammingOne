934ede9df24407e33f23bde1ea699d3a95e3bf3c
Alexander Vasil <alexandervasil840@gmail.com>
2019-09-05 11:22:26 -0400

    nothing changed

--- TotalPriceCalculator/src/totalpricecalculator/TotalPriceCalculator.java
+++ TotalPriceCalculator/src/totalpricecalculator/TotalPriceCalculator.java
@@ -9,21 +9,21 @@ public class TotalPriceCalculator {
     public static void main(String[] args) {
         //Sets the menu item price, federal tax, provincal and tip percentage rate.
         double menuPrice = 20; 
-        double federalTaxRatio =0.05;
-        double provincialTaxRatio = 0.09975;
+        double federalTaxRate =0.05;
+        double provincialTaxRate = 0.09975;
         double tipPercentage = 0.15; 
         //calculates the federal, provincial and the total amount after tax.
-        double federalTax = menuPrice * federalTaxRatio;
-        double procincialTax = menuPrice * provincialTaxRatio;
+        double federalTax = menuPrice * federalTaxRate;
+        double procincialTax = menuPrice * provincialTaxRate;
         double totalTax = federalTax + procincialTax;
-        
+        //calculates the total amount for tips, final price, and final price with tips
         double tips = (menuPrice+totalTax) * tipPercentage; 
         double finalPrice = menuPrice +totalTax;
         double finalPriceTips = menuPrice + totalTax + tips;
-        //prints final price which includes taxes and tips.
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");      
-        System.out.println("Federal tax: " + federalTaxRatio*100 + "%");        
-        System.out.println("Provincial tax: " + provincialTaxRatio*100+ "%");
+        //prints the federal and provincial tax rate, the total amount of the tips, and the total price with and without tips.
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Federal tax: " + federalTaxRate*100 + "%");
+        System.out.println("Provincial tax: " + provincialTaxRate*100+ "%");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Tips: $"+tips);
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
