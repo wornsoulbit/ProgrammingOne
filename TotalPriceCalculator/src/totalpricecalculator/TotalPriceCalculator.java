b1cacd26702a567206ccf57d4d137cf511a83797
Alexander Vasil <alexandervasil840@gmail.com>
2019-09-05 11:12:26 -0400

    created the Print lines that show total taxes (federal and provincial seperately, the total amount for the tips and the total price before and after tips.

--- TotalPriceCalculator/src/totalpricecalculator/TotalPriceCalculator.java
+++ TotalPriceCalculator/src/totalpricecalculator/TotalPriceCalculator.java
@@ -9,12 +9,26 @@ public class TotalPriceCalculator {
     public static void main(String[] args) {
         //Sets the menu item price, federal tax, provincal and tip percentage rate.
         double menuPrice = 20; 
-        double fedTax =0.05;
-        double provincalTax = 0.09975;
+        double federalTaxRatio =0.05;
+        double provincialTaxRatio = 0.09975;
         double tipPercentage = 0.15; 
-        //prints Total price including tips, then prints the total cost.
-        System.out.print("Total Price including tips: ");
-        System.out.println(menuPrice*(1+fedTax+provincalTax)*(1+tipPercentage));
+        //calculates the federal, provincial and the total amount after tax.
+        double federalTax = menuPrice * federalTaxRatio;
+        double procincialTax = menuPrice * provincialTaxRatio;
+        double totalTax = federalTax + procincialTax;
+        
+        double tips = (menuPrice+totalTax) * tipPercentage; 
+        double finalPrice = menuPrice +totalTax;
+        double finalPriceTips = menuPrice + totalTax + tips;
+        //prints final price which includes taxes and tips.
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");      
+        System.out.println("Federal tax: " + federalTaxRatio*100 + "%");        
+        System.out.println("Provincial tax: " + provincialTaxRatio*100+ "%");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Tips: $"+tips);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Total Price without tips: $" + finalPrice);
+        System.out.println("Total Price including tips: $" + finalPriceTips);
         
         //Sets and prints the value of num
 //        double num = (3%5+1.1)*(28/10);
