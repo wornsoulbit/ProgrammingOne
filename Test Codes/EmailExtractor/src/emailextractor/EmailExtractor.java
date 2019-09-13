1a7c9d82fea74190141d7ab27f75f948a4e31bc8
Alexander Vasil <alexandervasil840@gmail.com>
2019-09-13 15:32:49 -0400

    created an email extractor in which it gets the address and the service provider of the email

---
+++ Test Codes/EmailExtractor/src/emailextractor/EmailExtractor.java
@@ -0,0 +1,27 @@
+package emailextractor;
+
+import java.util.Scanner;
+
+/**
+ * Find the Email address, and the host name.
+ * @author Alexander Vasil
+ */
+public class EmailExtractor {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please enter your email address: ");
+        String email = input.nextLine();
+        int idxAtSign = email.indexOf('@');
+        
+        String emailAddress = email.substring(0, idxAtSign);
+        String hostProvider = email.substring(idxAtSign + 1);
+        
+        System.out.printf("%-21s: %s\n", "Your email address is", emailAddress);
+        System.out.printf("%-21s: %s\n", "The host provider is", hostProvider);
+
+    }
+    
+}
