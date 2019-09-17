cb6dd63f9b82625ac6cd1f40b0f021ca08a18a24
Alexander Vasil <alexandervasil840@gmail.com>
2019-09-17 13:37:40 -0400

    Created and finished ParkingPriceCalculator

--- Assignment02/SimpleStatisticCalculator/src/simplestatisticcalculator/SimpleStatisticCalculator.java
+++ Assignment02/SimpleStatisticCalculator/src/simplestatisticcalculator/SimpleStatisticCalculator.java
@@ -1,5 +1,7 @@
 package simplestatisticcalculator;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author Alexander Vasil
@@ -8,6 +10,16 @@ public class SimpleStatisticCalculator {
 
     public static void main(String[] args) {
         
+        Scanner input = new Scanner(System.in);
+        
+//        System.out.print("Please input 5 numbers seperated by spaces: ");
+//        String numbers = input.nextLine();
+        String numbers = "5 6 7 8 9";
+        
+        int spaceIdx = numbers.indexOf(' ');
+        
+        System.out.println(numbers);
+        System.out.println(spaceIdx);
     }
     
 }
