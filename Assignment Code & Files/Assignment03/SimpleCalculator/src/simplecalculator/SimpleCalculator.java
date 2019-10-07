1f76d9728567ef804d20f4b38cb834c55b854df5
Alexander Vasil <alexandervasil840@gmail.com>
2019-10-07 14:19:53 -0400

    Started to work on the first file

--- Assignment Code & Files/Assignment03/SimpleCalculator/src/simplecalculator/SimpleCalculator.java
+++ Assignment Code & Files/Assignment03/SimpleCalculator/src/simplecalculator/SimpleCalculator.java
@@ -1,21 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package simplecalculator;
 
+import java.util.Scanner;
+
 /**
+ * A simple addition, subtraction, multiplication and division calculator.
  *
- * @author cstuser
+ * @author Alexander Vasil
  */
 public class SimpleCalculator {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter two numbers, seperated by a space: ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        System.out.print("Please choose an operation: (+, -, *, /): ");
+        char oper = input.next().charAt(0);
+        input.nextLine();
+        System.out.print("Please enter a formula: e.g. \"3.14 * 2\": ");
+        String formula = input.nextLine();
+        
+        System.out.println(calcResult("3.14 * 2"));
     }
-    
+
+    /**
+     * Calculates the addition of two numbers.
+     *
+     * @param num1 number 1.
+     * @param num2 number 2.
+     * @return the addition of num1 and num2.
+     */
+    public static double calcResult(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    /**
+     * calculate based on the operator with two numbers.
+     *
+     * @param num1 number 1.
+     * @param num2 number 2.
+     * @param oper the operator (e.g. +, -, *, /).
+     * @return the result based on the operator.
+     */
+    public static double calcResult(double num1, double num2, char oper) {
+        if (oper == '+') {
+            return num1 + num2;
+        } else if (oper == '-') {
+            return num1 - num2;
+        } else if (oper == '*') {
+            return num1 * num2;
+        } else {
+            return num1 / num2;
+        }
+    }
+
+    public static double calcResult(String formula) {
+        int firstNumLength = formula.indexOf(' ');
+        double firstNum = Integer.parseInt(formula.substring(0, firstNumLength + 1));
+        return firstNum;
+    }
 }
