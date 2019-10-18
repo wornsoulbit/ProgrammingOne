dbf072acafe270c9c45eeea526fdd0df749d5d3d
Alex Vasil <alexandervasil840@gmail.com>
2019-10-18 14:58:23 -0400

    Created a new project which is a simple version of a ATM which supports: 1. Withdraw. 2. Deposit. 3. Display balance.

---
+++ Test Codes/ATM/src/atm/ATM.java
@@ -0,0 +1,21 @@
+package atm;
+
+import java.util.Scanner;
+
+/**
+ * A simple version of a ATM, which supports the following:
+ * 1. Withdraw.
+ * 2. Deposit.
+ * 3. Display balance.
+ * 
+ * @author Alex Vasil
+ */
+public class ATM {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+    
+    
+}
