c0780ed54b8544332cdd12625ccd5d14c9154586
Alex Vasil <alexandervasil840@gmail.com>
2019-10-28 11:28:38 -0400

    Using constructor for the first time.

---
+++ Test Codes/Animal/src/animal/Animal.java
@@ -0,0 +1,16 @@
+package animal;
+
+/**
+ *
+ * @author Alex Vasil
+ */
+public class Animal {
+
+    public static void main(String[] args) {
+        double min = 4;
+        Dog d1 = new Dog();
+        Dog d2 = new Dog("Copain", 3, 'm', "mixed" , "Black & Brown");
+        Dog d3 = new Dog("Lucy", 2, 'f', "Chiwawa");
+    }
+
+}
