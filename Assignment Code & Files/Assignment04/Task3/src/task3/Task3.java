bae788df7329187edf2310af37483934e18eb63a
Alexander Vasil <alexandervasil840@gmail.com>
2019-10-28 20:06:23 -0400

    Completed Task 3 which calculates the salary of a person over x amount of time. if x <= 3 then the salary increases by 3% yearly, if x > 3 <= 10 it increases by 5%, if x > 10 it increases by 8%.

--- Assignment Code & Files/Assignment04/Task3/src/task3/Task3.java
+++ Assignment Code & Files/Assignment04/Task3/src/task3/Task3.java
@@ -1,29 +1,40 @@
 package task3;
 
 /**
- * Calculates the salary of an employee year over year with an increase in the 
- * workers salary
- * .
+ * Calculates the salary of an employee year over year with an increase in the
+ * workers salary .
+ *
  * @author Alex Vasil
  */
 public class Task3 {
 
     public static void main(String[] args) {
-
+        System.out.printf(".2f", calcFutureSalary(30000, 5));
     }
-    
+
     /**
      * Calculates the future salary of an employee based off of how many years
      * they've worked for the company.
-     * 
+     *
      * @param currentSal the employee's current yearly salary.
      * @param year the number of years the employee worked for the employer.
-     * @return the future salary based on how many years the employee worked 
-     * for the company.
+     * @return the future salary based on how many years the employee worked for
+     * the company.
      */
     public static double calcFutureSalary(double currentSal, int year) {
-        for (int i = year; i < 2; i++) {
-            
+        double yearIntOne = 0.03;
+        double yearIntTwo = 0.05;
+        double yearIntThree = 0.08;
+
+        for (int i = 1; i <= year; i++) {
+            if (i <= 3) {
+                currentSal += currentSal * yearIntOne;
+            } else if (i > 3 && i <= 10) {
+                currentSal += currentSal * yearIntTwo;
+            } else {
+                currentSal += currentSal * yearIntThree;
+            }
         }
+        return currentSal;
     }
 }
