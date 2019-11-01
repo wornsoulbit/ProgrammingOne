815be29a5790efe230d850d0274fe68667a89f84
Alex Vasil <alexandervasil840@gmail.com>
2019-11-01 13:37:07 -0400

    renamed all the projects to more appropriate names

---
+++ Assignment Code & Files/assignment04/CalcSalary/src/calcsalary/CalcSalary.java
@@ -0,0 +1,40 @@
+package calcsalary;
+
+/**
+ * Calculates the salary of an employee year over year with an increase in the
+ * workers salary .
+ *
+ * @author Alex Vasil
+ */
+public class CalcSalary {
+
+    public static void main(String[] args) {
+        System.out.printf(".2f", calcFutureSalary(30000, 5));
+    }
+
+    /**
+     * Calculates the future salary of an employee based off of how many years
+     * they've worked for the company.
+     *
+     * @param currentSal the employee's current yearly salary.
+     * @param year the number of years the employee worked for the employer.
+     * @return the future salary based on how many years the employee worked for
+     * the company.
+     */
+    public static double calcFutureSalary(double currentSal, int year) {
+        double yearIntOne = 0.03;
+        double yearIntTwo = 0.05;
+        double yearIntThree = 0.08;
+
+        for (int i = 1; i <= year; i++) {
+            if (i <= 3) {
+                currentSal += currentSal * yearIntOne;
+            } else if (i > 3 && i <= 10) {
+                currentSal += currentSal * yearIntTwo;
+            } else {
+                currentSal += currentSal * yearIntThree;
+            }
+        }
+        return currentSal;
+    }
+}
---
+++ Assignment Code & Files/assignment04/NumToLetterScore/src/numtoletterscore/NumToLetterScore.java
@@ -0,0 +1,75 @@
+package numtoletterscore;
+
+/**
+ * Takes a score between 0 and 100 and gives a letter based on the score, e.g.
+ * 95 > 'a'.
+ * 
+ * @author Alex Vasil
+ */
+public class NumToLetterScore {
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * takes a digit score in [0, 100], then calculate the letter score based on
+     * it.
+     *
+     * @param digitScore the input score from 0 to 100.
+     * @return a char based on the inputed score.
+     */
+    public static char toLetterScore(double digitScore) {
+
+        if (digitScore >= 90 && digitScore <= 100) {
+            return 'a';
+        } else if (digitScore < 90 && digitScore >= 80) {
+            return 'b';
+        } else if (digitScore < 80 && digitScore >= 70) {
+            return 'c';
+        } else if (digitScore < 70 && digitScore >= 60) {
+            return 'd';
+        } else if (digitScore < 60) {
+            return 'f';
+        } else {
+            return ' ';
+        }
+    }
+
+    /**
+     * Takes a letter score, then print “Excellent” for ‘a’ or ‘A’, “Well done”
+     * for ‘b’ or ‘B’, ‘c’ or ‘C’, “Passed” for ‘d’ or ‘D’, and “better try
+     * again" for ‘f’ or ‘F’. For all the other letters, print “Invalid grade”
+     *
+     * @param letterScore the score a person got on a test.
+     */
+    public static void printMessage(char letterScore) {
+
+        switch (letterScore) {
+            case 'a':
+                System.out.println("Excellent!");
+                break;
+            case 'b':
+            case 'c':
+                System.out.println("Well done.");
+                break;
+            case 'd':
+            case 'f':
+                System.out.println("Better luck next time.");
+                break;
+            default:
+                System.out.println("Invaild Grade.");
+                break;
+        }
+
+//        if (letterScore == 'a') {
+//            System.out.println("Excellent!");
+//        } else if (letterScore == 'b' || letterScore == 'c') {
+//            System.out.println("Well done.");
+//        } else if (letterScore == 'd' || letterScore == 'f'){
+//            System.out.println("Better luck next time.");
+//        } else {
+//            System.out.println("Invaild Grade.");
+//        }
+    }
+}
