5d6a535a67f9d17aa36af575bea5cf16d10d3c41
Alex Vasil <alexandervasil840@gmail.com>
2019-11-04 10:47:18 -0500

    Added documentation to the code.

--- Exam Codes/Exam02/CopyString/src/copystring/CopyString.java
+++ Exam Codes/Exam02/CopyString/src/copystring/CopyString.java
@@ -1,16 +1,23 @@
 package copystring;
 
 /**
- *
+ * Take an input string e.g. "Hello" and repeat each letter in that string an
+ * additional time to get the output string e.g. "HHeelllloo".
  * @author Alex Vasil
  */
 public class CopyString {
 
     public static void main(String[] args) {
-        System.out.println(repeatChar("Hello"));
+        System.out.println(repeatChar("point"));
     }
 
-
+    /**
+     * Repeats each character in a given string an additional time e.g. "Hi" to
+     * "HHii"
+     * 
+     * @param strIn the inputed string.
+     * @return the String with the additional characters in it.
+     */
     public static String repeatChar(String strIn) {
         String strOut = "";
         for (int i = 0; i < strIn.length(); i++) {
