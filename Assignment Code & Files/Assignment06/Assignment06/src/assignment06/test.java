ef5e1e17bf46475326294e5d6fff23a3c9065c53
Alex Vasil <alexandervasil840@gmail.com>
2019-11-12 14:03:26 -0500

    The test file to test the constructors, to make sure they're working as intended.

---
+++ Assignment Code & Files/Assignment06/Assignment06/src/assignment06/test.java
@@ -0,0 +1,14 @@
+package assignment06;
+
+/**
+ *
+ * @author Alex Vasil
+ */
+public class test {
+    
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle();
+        System.out.println(r1.toString());
+    }
+    
+}
