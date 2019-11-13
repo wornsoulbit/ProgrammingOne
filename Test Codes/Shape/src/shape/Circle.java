da304aefabac4342c4170838bf0f833a1842c00f
Alex Vasil <alexandervasil840@gmail.com>
2019-11-13 17:20:46 -0500

    Added Javadocs to the class.

--- Test Codes/Shape/src/shape/Circle.java
+++ Test Codes/Shape/src/shape/Circle.java
@@ -3,13 +3,16 @@ package shape;
 import java.util.Random;
 
 /**
- *
+ * A class of circle
  * @author Alex Vasil
  */
 public class Circle {
     
     private double radius;
 
+    /**
+     * Default constructor that gives a random radius.
+     */
     public Circle() {
         Random rand = new Random();
         int numRange = 10; //The range of numbers e.g. [0, 10[
@@ -17,22 +20,42 @@ public class Circle {
         this.radius = rand.nextInt(numRange) + minNum;
     }
 
+    /**
+     * Constructor with a specific radius.
+     * @param radius the radius.
+     */
     public Circle(double radius) {
         this.radius = radius;
     }
 
+    /**
+     * Copy constructor.
+     * @param anotherCircle the target circle.
+     */
     public Circle(Circle anotherCircle) {
         this.radius = anotherCircle.radius;
     }
 
+    /**
+     * Calculates the area of a circle.
+     * @return the area of the circle.
+     */
     public double calcArea() {
         return Math.pow((Math.PI * this.radius), 2);
     }
     
+    /**
+     * Calculates the circumference of a circle.
+     * @return the circumference of the circle.
+     */
     public double calcCircumference() {
         return 2 * Math.PI * this.radius;
     }
     
+    /**
+     * To generate a string to represent the circle.
+     * @return a generated string for circle.
+     */
     @Override
     public String toString() {
         String message = String.format("%-13s: %.2f\n", "Radius", radius);
@@ -42,14 +65,35 @@ public class Circle {
         return message;
     }
     
+    /**
+     * To check if two circles are the same.
+     * @param anotherCircle the other circle to compare.
+     * @return To check if two circles are the same.
+     */
     public boolean equals(Circle anotherCircle) {
         return this.radius == anotherCircle.radius;
     }
     
+    /**
+     * A copy of a circle.
+     * @return a copy of a circle.
+     */
+    public Circle clone() {
+        return new Circle(this);
+    }
+    
+    /**
+     * Getter.
+     * @return the radius of the circle.
+     */
     public double getRadius() {
         return radius;
     }
 
+    /**
+     * Setter.
+     * @param radius sets the radius of the circle.
+     */
     public void setRadius(double radius) {
         this.radius = radius;
     }
--- Test Codes/Shape/src/shape/Test.java
+++ Test Codes/Shape/src/shape/Test.java
@@ -6,6 +6,10 @@ package shape;
  */
 public class Test {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Circle c1 = new Circle();
         System.out.println(c1);
