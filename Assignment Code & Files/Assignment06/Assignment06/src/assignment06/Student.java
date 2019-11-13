37b55f48554bfb6756b193ab7a5aa81d8d2d308d
Alex Vasil <alexandervasil840@gmail.com>
2019-11-13 14:28:58 -0500

    Added basic Javadocs to the file without any addition information in them.

--- Assignment Code & Files/Assignment06/Assignment06/src/assignment06/Student.java
+++ Assignment Code & Files/Assignment06/Assignment06/src/assignment06/Student.java
@@ -14,6 +14,9 @@ public class Student {
     private Course course2;
     private Course course3;
 
+    /**
+     *
+     */
     public Student() {
         this.name = "";
         this.gender = "";
@@ -23,6 +26,12 @@ public class Student {
         this.course3 = new Course();
     }
 
+    /**
+     *
+     * @param name
+     * @param gender
+     * @param email
+     */
     public Student(String name, String gender, String email) {
         this.name = name;
         this.gender = gender;
@@ -32,6 +41,15 @@ public class Student {
         this.course3 = new Course();
     }
 
+    /**
+     *
+     * @param name
+     * @param gender
+     * @param email
+     * @param course1
+     * @param course2
+     * @param course3
+     */
     public Student(String name, String gender, String email, Course course1,
             Course course2, Course course3) {
         this.name = name;
@@ -42,6 +60,10 @@ public class Student {
         this.course3 = course3;
     }
 
+    /**
+     *
+     * @return
+     */
     public int calcPassedCourseNum() {
         int passedCourses = 0;
         if (this.course1.isPassed())
@@ -56,6 +78,10 @@ public class Student {
         return passedCourses;
     }
 
+    /**
+     *
+     * @return
+     */
     public double calcTotalCredit() {
         double totalCredits = 0;
         if (this.course1.isPassed()) 
@@ -70,6 +96,10 @@ public class Student {
         return totalCredits;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isValidEmail() {
         int atSignIdx = this.email.indexOf("@");
         int atDotIdx = this.email.lastIndexOf("."); // ww.alex@gmail.qc.ca
@@ -81,8 +111,10 @@ public class Student {
                 && atDotIdx != this.email.length() - 1;
         }
 
-    
-
+    /**
+     *
+     * @param otherStudent
+     */
     public Student(Student otherStudent) {
         this.name = otherStudent.name;
         this.gender = otherStudent.gender;
@@ -92,11 +124,20 @@ public class Student {
         this.course3 = otherStudent.course3;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public Student clone() {
         return new Student(this);
     }
 
+    /**
+     *
+     * @param otherStudent
+     * @return
+     */
     public boolean equals(Student otherStudent) {
         return this.name.equals(otherStudent.name)
                 && this.gender.equals(otherStudent.gender)
@@ -106,6 +147,10 @@ public class Student {
                 && this.course3 == otherStudent.course3;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         String message = String.format("%-15s: %s\n", "Name", name);
@@ -135,50 +180,98 @@ public class Student {
         return message;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @return
+     */
     public Course getCourse1() {
         return course1;
     }
 
+    /**
+     *
+     * @return
+     */
     public Course getCourse2() {
         return course2;
     }
 
+    /**
+     *
+     * @return
+     */
     public Course getCourse3() {
         return course3;
     }
 
+    /**
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @param gender
+     */
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    /**
+     *
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @param course1
+     */
     public void setCourse1(Course course1) {
         this.course1 = course1;
     }
 
+    /**
+     *
+     * @param course2
+     */
     public void setCourse2(Course course2) {
         this.course2 = course2;
     }
 
+    /**
+     *
+     * @param course3
+     */
     public void setCourse3(Course course3) {
         this.course3 = course3;
     }
